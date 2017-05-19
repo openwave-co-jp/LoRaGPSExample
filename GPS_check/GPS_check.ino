6a63df953a412db8267e146d3229a8e9eca81467
ASUZUKI\淳史 <asuzuki@openwave.co.jp>
2017-05-19 22:03:38 +0900

    first commit

---
+++ GPS_check/GPS_check.ino
@@ -0,0 +1,130 @@
+#include <SPI.h>
+#include <LoRa.h>
+//#include <RH_RF95.h>
+#include <SoftwareSerial.h>
+#define RX A1
+#define TX A2
+
+char msg[100];
+int msgLength;
+SoftwareSerial mySerial(RX,TX);
+//RH_RF95 rf95;
+//float frequency = 923.6;
+
+void setup() {
+  Serial.begin(9600);
+  while(!Serial);
+  Serial.println("Starting");
+  
+  Serial.println("LoRa Sender");
+//  if (!rf95.init())
+//    Serial.println("init failed");
+//  // Setup ISM frequency
+//  rf95.setFrequency(frequency);
+//  // Setup Power,dBm
+//  rf95.setTxPower(23);
+//  RH_RF95::ModemConfig config;
+//  config.reg_1d = 0b01110000 | 0b0100; // Bw125 | Cr46
+//  config.reg_1e = 0b10010000 | 0b0100; // Sf512 | CRCon
+//  config.reg_26 = 0b00000000; // default.
+//  rf95.setModemRegisters(&config);
+
+  if (!LoRa.begin(9236E5)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+  LoRa.setSpreadingFactor(9);
+  LoRa.setSignalBandwidth(125E3);
+  LoRa.setCodingRate4(6);
+  LoRa.crc();
+  LoRa.setTxPower(22);
+  mySerial.begin(9600);
+  pinMode(A0, INPUT);
+}
+
+void loop() {
+  if(mySerial.available()){
+    char c = mySerial.read();
+    if( -1 != c )
+    {
+      //Serial.print(c);
+         
+      if('$'==(char)c){
+        Serial.print(msg);
+        if(msg[0]=='$' && 
+           msg[1]=='G' && 
+           msg[2]=='P' && 
+           msg[3]=='G' && 
+           msg[4]=='G' &&
+           msg[5]=='A'){
+//            char *tp;
+//            int cnt=0;
+//            /* スペース.を区切りに文字列を抽出 */   
+//            tp = strtok( msg, "," );
+//            char latSign,longSign,*stopstring;
+//            double utc;
+//            float latVal,longVal;
+//            
+//            while ( tp != NULL ) {
+//              cnt++;
+//              if(cnt==2){
+//                Serial.print("UTC:");
+//                utc=strtod(tp,&stopstring);
+//                Serial.println(utc);
+//                
+//              }
+//              if(cnt==3){
+//                Serial.print("lat:");
+//                latVal=atof(tp);
+//                Serial.println(latVal);
+//                
+//              }
+//              if(cnt==4){
+//                Serial.print("lat sign:");
+//                latSign=(char)tp;
+//                Serial.println(latSign);
+//                
+//              }
+//              if(cnt==5){
+//                Serial.print("long:");
+//                longVal=atof(tp);
+//                Serial.println(longVal);
+//                
+//              }
+//              if(cnt==6){
+//                Serial.print("long sign:");
+//                longSign=(char)tp;
+//                Serial.println(longSign);
+//                
+//              }
+//              tp = strtok( NULL,"," );
+//            }
+            // send packet
+//            rf95.send(msg, strlen((char*)msg));
+            LoRa.beginPacket();
+//            LoRa.print(utc);
+//            LoRa.print(",");
+//            LoRa.print(latVal);
+//            LoRa.print(",");
+//            LoRa.print(latSign);
+//            LoRa.print(",");
+//            LoRa.print(longVal);
+//            LoRa.print(",");
+//            LoRa.print(longSign);
+            LoRa.print(msg);
+            LoRa.endPacket();
+            delay(5000);
+        }
+        *msg = "";
+        msgLength=0;
+        msg[msgLength] = c;
+        Serial.println();
+      }else {
+        msgLength++;
+        msg[msgLength] = c;
+      }
+    }
+  }
+  
+  //Serial.println(digitalRead(A0));
+}
---
+++ GPS_check_GW/GPS_check_GW.ino
@@ -0,0 +1,113 @@
+#include <SPI.h>
+#include <LoRa.h>
+//#include <Console.h>
+#include "ThingSpeak.h"
+#include "YunClient.h"
+YunClient client;
+
+//RH_RF95 rf95;
+//float frequency = 923.6;
+unsigned long myChannelNumber = 000000;
+const char * myWriteAPIKey = "XXXXXXXXXXXXXXXX";
+
+void setup() {
+  Bridge.begin(115200);
+//  Console.begin();
+//  while(!Console);
+  ThingSpeak.begin(client);
+//  Console.println("LoRa Receiver");
+  
+  if (!LoRa.begin(9236E5)) {
+//    Console.println("Starting LoRa failed!");
+    while (1);
+  }
+  LoRa.setSpreadingFactor(9);
+  LoRa.setSignalBandwidth(125E3);
+  LoRa.setCodingRate4(6);
+  LoRa.crc();
+  LoRa.setTxPower(22);
+}
+
+void loop() {
+  // try to parse packet
+  int packetSize = LoRa.parsePacket();
+  if (packetSize) {
+    // received a packet
+//    Console.print("Received packet '");
+//    Console.print("packetSize : ");
+//    Console.println(packetSize);
+    // read packet
+    int i = 0;
+    char message[256];
+    while (LoRa.available() && i < 256) {
+      message[i]=LoRa.read();
+      i++;
+    }
+//    Console.print(message);
+
+    
+    char *tp;
+    int cnt=0;
+    /* スペース.を区切りに文字列を抽出 */   
+    tp = strtok( message, "," );
+    char latSign,longSign,*stopstring;
+    double utc;
+    float latVal,longVal,altiVal;
+    
+    while ( tp != NULL ) {
+      cnt++;
+      if(cnt==2){
+//        Console.print("UTC:");
+//        utc=atof(tp);
+//        Console.println(utc);
+        
+      }
+      if(cnt==3){
+//        Console.print("lat:");
+        latVal=atof(tp);
+//        Console.println(latVal);
+        
+      }
+//      if(cnt==4){
+//        Console.print("lat sign:");
+//        latSign=(char)tp;
+//        Console.println(latSign);
+//        
+//      }
+      if(cnt==5){
+//        Console.print("long:");
+        longVal=atof(tp);
+//        Console.println(longVal);
+        
+      }
+//      if(cnt==6){
+//        Console.print("long sign:");
+//        longSign=(char)tp;
+//        Console.println(longSign);
+//        
+//      }
+      if(cnt==10){
+//        Console.print("alti sign:");
+        altiVal=atof(tp);
+//        Console.println(altiVal);
+      }
+      tp = strtok( NULL,"," );
+    }
+//    Console.println((double)utc);
+//    Console.println((float)latVal);
+//    Console.println((char)latSign);
+//    Console.println((float)longVal);
+//    Console.println((char)longSign);
+//    // print RSSI of packet
+//    Console.print("' with RSSI ");
+//    Console.println(LoRa.packetRssi());
+
+
+//  ThingSpeak.setField(1,ID);
+    ThingSpeak.setField(2,LoRa.packetRssi());
+    ThingSpeak.setField(3,latVal);
+    ThingSpeak.setField(4,longVal);
+    ThingSpeak.setField(5,altiVal);
+    ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);   // Send Data to IoT Server.
+  }
+}
